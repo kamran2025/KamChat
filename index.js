79720ea9d17443f7d4b38ce52a1292d933a7fc49
kamran2025 <144653562+kamran2025@users.noreply.github.com>
2025-05-24 20:04:28 +0530

    updated Api Key

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash"});
 
 async function main(input) {
   messages.push({ role: 'user', content: input });
